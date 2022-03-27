b9f349d8275fc2badb7dec0ee5378a54f60031b7
ineble <thdwltjq94@gmail.com>
2022-03-27 17:21:22 +0900

    수정일 최신화

--- src/main/java/com/sjs/exam/jpaBoard/article/controller/ArticleController.java
+++ src/main/java/com/sjs/exam/jpaBoard/article/controller/ArticleController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -34,11 +35,11 @@ public class ArticleController {
         if(article.isPresent()) {
             articleRepository.delete(article.get());
         }
-        return id + "번 게시물이 삭제되었습니다.";
+        return "%d번 게시물이 삭제되었습니다.".formatted(id);
     }
     @RequestMapping("doModify")
     @ResponseBody
-    public Article showModify(long id,String title,String body) {
+    public Article showModify(long id, String title, String body) {
         Article article = articleRepository.findById(id).get();
         if(title != null) {
             article.setTitle(title);
@@ -46,8 +47,11 @@ public class ArticleController {
         if(body != null) {
             article.setBody(body);
         }
+        article.setUpdateDate(LocalDateTime.now());
         articleRepository.save(article);
         return article;
     }
+
+
 
 }
